093110f1791e4c68af7086583db2c64ab8cd29ba
ShoNuffEnuff <108638570+ShoNuffEnuff@users.noreply.github.com>
2023-07-08 05:03:37 +0930

    Next commit

--- src/main/java/TwoUp/OG.java
+++ src/main/java/TwoUp/OG.java
@@ -30,7 +30,6 @@ import static TwoUp.LoginData.username;
 
 public class OG extends Application {
 
-
     static int winCount = 0;
     static int loseCount = 0;
     static int currentCount = 0;
@@ -55,6 +54,7 @@ public class OG extends Application {
     static Label cclabel;
     static Button bg;
     static BorderPane root;
+    static Button resetButton;
 
     private static final String DB_URL = "jdbc:mysql://localhost:3306/2up";
     private static final String DB_USER = "root";
@@ -63,6 +63,10 @@ public class OG extends Application {
     //private Stage primaryStage;
     private static Scene loginScene;
 
+    private static Image headsImage1;
+    private static Image tailsImage1;
+    static boolean headsCoin1 = true;  // Set initial value for coin1
+    static boolean headsCoin2 = true;  // Set initial value for coin2
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -95,6 +99,7 @@ public class OG extends Application {
     }
 
     public static Scene twoUp() {
+
         bg = new Button("Change BG");
         bg.setOnAction(event -> {
             Background newBackground = createBackground();
@@ -109,7 +114,6 @@ public class OG extends Application {
         score2 = new Label("Losses " + String.valueOf(loseCount));
         score2.setTextFill(Color.RED);
         score2.setFont(Font.font("Stencil", 18));
-
 
         //score2.setStyle("-fx-font-family: Arial; -fx-font-size: 20;");
         currentCount = winCount - loseCount;
@@ -161,8 +165,8 @@ public class OG extends Application {
         });
         lbutton = new Button("Leaderboard");
         lbutton.setOnAction(e -> leaderBoard());
-        scbutton = new Button("Change Button/Text Size");
-        double[] textSizeValues = {18, 24, 30};
+        scbutton = new Button("Change Size");
+        double[] textSizeValues = {12, 18, 24};
         final int[] currentSizeIndex = {0};
         scbutton.setOnAction(event -> {
             currentSizeIndex[0] = (currentSizeIndex[0] + 1) %
@@ -182,7 +186,7 @@ public class OG extends Application {
 
 
 
-        sbutton = new Button("Save");
+        //sbutton = new Button("Save");
 
         passwordField = new PasswordField();
         passwordField.setPromptText("Password");
@@ -190,10 +194,10 @@ public class OG extends Application {
         playerName.setFont(new Font("Old English Text MT", 48));
         playerName.setTextFill(Color.WHITE);
         //playerName.setStyle("-fx-font-family: Arial; -fx-font-size: 20;");
-        Button registerButton = new Button("Register");
+        //Button registerButton = new Button("Register");
         //registerButton.setOnAction(e -> registerUser());
 
-        Button loginButton = new Button("Login");
+        //Button loginButton = new Button("Login");
         //loginButton.setOnAction(e -> loginUser());
 
         //Button logoutButton = new Button("Logout");
@@ -202,7 +206,7 @@ public class OG extends Application {
         GridPane gridPane = new GridPane();
         GridPane scorePane = new GridPane();
         GridPane buttonPane = new GridPane();
-        Button resetButton = new Button("Reset Colours");
+        resetButton = new Button("Reset Colours");
         resetButton.setOnAction(event -> {
             // Reset the colors to their original values
             button.setTextFill(Color.BLACK);
@@ -221,19 +225,19 @@ public class OG extends Application {
         gridPane.setAlignment(Pos.CENTER_LEFT);
         buttonPane.setHgap(0);
         buttonPane.setVgap(5);
-        buttonPane.add(bg, 0, 0);
-        buttonPane.add(ccbutton, 1, 6);
-        buttonPane.add(scbutton, 0, 6);
-        //gridPane.add(loginButton, 0, 3);
+        buttonPane.add(bg, 0, 1);
+        buttonPane.add(ccbutton, 0, 2);
+        buttonPane.add(scbutton, 0, 3);
+        //buttonPane.add(resetButton, 0, 4);
         gridPane.add(button, 0, 6);
         gridPane.add(button2, 1, 6);
-        buttonPane.add(lbutton, 2, 6);
+        buttonPane.add(lbutton, 3, 0);
         //gridPane.add(sbutton, 2, 1);
         scorePane.add(score, 1, 2);
         scorePane.add(score2, 2, 2);
         scorePane.add(playerName, 0, 1, 3, 1);
         scorePane.add(score3,3,2 );
-        buttonPane.add(logoutButton, 0, 7);
+        buttonPane.add(logoutButton, 1, 0);
 
         coin1 = new Circle(70);
         coin2 = new Circle(70);
@@ -241,7 +245,23 @@ public class OG extends Application {
         coin2.setFill(new ImagePattern(new Image("C:\\Users\\Sho\\IdeaProjects\\TwoUp\\src\\main\\resources\\TwoUp\\img\\gold2.png"), 0, 0, 1, 1, true));
         coin1.setStroke(Color.BLACK);
         coin2.setStroke(Color.BLACK);
+        //Region buttonBackground = new Region();
+        //buttonBackground.setStyle("-fx-background-color: rgba(0, 0, 0, 0.5);");
+        //buttonBackground.setPrefSize(300, 200);
 
+        HBox buttonBox2 = new HBox();
+        buttonBox2.setSpacing(10);
+        buttonBox2.getChildren().addAll(bg, scbutton, ccbutton, lbutton, logoutButton, resetButton);
+        buttonBox2.setAlignment(Pos.BOTTOM_LEFT);
+        buttonBox2.setPadding(new Insets(10));
+
+
+
+
+        //HBox buttonsBox = new HBox();
+        //buttonsBox.getChildren().add(buttonBox2); // Remove buttonBackground from the children list
+        //buttonsBox.setSpacing(10); // Set spacing between buttons
+        //buttonsBox.setAlignment(Pos.TOP_LEFT);
         HBox coinBox = new HBox(10);
         coinBox.getChildren().addAll(coin1, coin2);
         coinBox.setAlignment(Pos.BOTTOM_CENTER);
@@ -254,24 +274,61 @@ public class OG extends Application {
         scoreBackground.setPrefSize(300, 200);
 
         StackPane scoreBox = new StackPane();
-        scoreBox.getChildren().addAll(scoreBackground, scoreContent);
+        scoreBox.getChildren().addAll(scoreBackground, scoreContent, buttonBox2);
         HBox buttonBox = new HBox(10);
         buttonBox.getChildren().addAll(button, button2);
         buttonBox.setAlignment(Pos.CENTER);
         VBox buttonCoinBox = new VBox(10);
         buttonCoinBox.getChildren().addAll(buttonBox, coinBox);
         buttonCoinBox.setAlignment(Pos.CENTER);
+        // Create a new VBox to hold the buttons in rows of three
+        //VBox buttonsBox = new VBox(10);
+        //buttonsBox.setAlignment(Pos.TOP_LEFT);
+
+// Create a new HBox to hold the buttons in rows of three
+        // Create the first row of buttons
+        HBox firstRow = new HBox(10);
+        firstRow.setAlignment(Pos.CENTER);
+        firstRow.getChildren().addAll(ccbutton, scbutton, resetButton);
+
+// Create the second row of buttons
+        HBox secondRow = new HBox(10);
+        secondRow.setAlignment(Pos.CENTER);
+        secondRow.getChildren().addAll(bg, lbutton, logoutButton);
+
+// Create the VBox to hold the rows of buttons
+        VBox rowsBox = new VBox(10);
+        rowsBox.setAlignment(Pos.BOTTOM_CENTER);
+        rowsBox.getChildren().addAll(firstRow, secondRow);
+
+        buttonBox2.getChildren().add(rowsBox);
+
+        scoreBox.getChildren().add(rowsBox);
+
+        /*Region buttonBackground = new Region();
+        buttonBackground.setStyle("-fx-background-color: rgba(0, 0, 0, 0.5);");
+        buttonBackground.setPrefSize(300, 200);
+
+        VBox buttonBox2 = new VBox();
+        buttonBox2.setSpacing(10);
+        buttonBox2.setStyle("-fx-background-color: rgba(0, 0, 0, 0.5);");
+        buttonBox2.getChildren().addAll(bg, scbutton, ccbutton, lbutton, logoutButton, resetButton);
+        buttonBox2.setAlignment(Pos.BOTTOM_LEFT);
+        buttonBox2.setPadding(new Insets(10));
+
+        HBox buttonsBox = new HBox();
+        buttonsBox.getChildren().add(buttonBox2); // Remove buttonBackground from the children list
+        buttonsBox.setSpacing(10); // Set spacing between buttons
+        buttonsBox.setAlignment(Pos.TOP_LEFT);*/
+
 
         root = new BorderPane();
         root.setBackground(createBackground());
-        root.setCenter(gridPane);
-        root.setLeft(buttonPane);
-        root.setBottom(buttonCoinBox);
-        root.setRight(scoreBox); // Add the scoreBox to the right side of the BorderPane
-        BorderPane.setAlignment(gridPane, Pos.CENTER);
-        BorderPane.setAlignment(buttonPane, Pos.CENTER);
-        BorderPane.setAlignment(coinBox, Pos.BOTTOM_CENTER);
-        BorderPane.setAlignment(scoreBox, Pos.CENTER);
+        root.setCenter(buttonCoinBox);
+        //root.setLeft(buttonsBox);
+        //root.setBottom();
+        root.setRight(scoreBox);
+
         Scene scene = new Scene(root, 1000, 500);
         primaryStage.setTitle("2UP Game");
         primaryStage.setScene(scene);
@@ -284,10 +341,6 @@ public class OG extends Application {
     private static void leaderBoard() {
         new LeaderBoard();
     }
-
-
-
-
 
     private static int imageIndex = -1; // Initialize with -1 to start at index 0
 
@@ -313,14 +366,29 @@ public class OG extends Application {
                 null,
                 new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, true)
         );
-
+        // Update the heads and tails images for coin1 and coin2 based on the background
+        if (imagePath.equals("C:\\Users\\Sho\\IdeaProjects\\TwoUp\\src\\main\\resources\\TwoUp\\img\\aussieflag.gif")) {
+            headsImage1 = new Image("C:\\Users\\Sho\\IdeaProjects\\TwoUp\\src\\main\\resources\\TwoUp\\img\\gold1.png");
+            tailsImage1 = new Image("C:\\Users\\Sho\\IdeaProjects\\TwoUp\\src\\main\\resources\\TwoUp\\img\\gold2.png");
+        } else if (imagePath.equals("C:\\Users\\Sho\\IdeaProjects\\TwoUp\\src\\main\\resources\\TwoUp\\img\\aussieflagGS.gif")) {
+            headsImage1 = new Image("C:\\Users\\Sho\\IdeaProjects\\TwoUp\\src\\main\\resources\\TwoUp\\img\\silver1.jpg");
+            tailsImage1 = new Image("C:\\Users\\Sho\\IdeaProjects\\TwoUp\\src\\main\\resources\\TwoUp\\img\\silver2.jpg");
+        } else if (imagePath.equals("C:\\Users\\Sho\\IdeaProjects\\TwoUp\\src\\main\\resources\\TwoUp\\img\\ausflagGS.jpg")) {
+            headsImage1 = new Image("C:\\Users\\Sho\\IdeaProjects\\TwoUp\\src\\main\\resources\\TwoUp\\img\\silver1.jpg");
+            tailsImage1 = new Image("C:\\Users\\Sho\\IdeaProjects\\TwoUp\\src\\main\\resources\\TwoUp\\img\\silver2.jpg");
+        } else {
+            headsImage1 = new Image("C:\\Users\\Sho\\IdeaProjects\\TwoUp\\src\\main\\resources\\TwoUp\\img\\gold1.png");
+            tailsImage1 = new Image("C:\\Users\\Sho\\IdeaProjects\\TwoUp\\src\\main\\resources\\TwoUp\\img\\gold2.png");
+        }
+        // Update the coin images
+        headsCoin2 = false;
+        updateCoinImages(headsCoin1, headsCoin2);
         return new Background(backgroundImg);
     }
 
     private static void resetBackground() {
         imageIndex = -1; // Reset the image index to 0
     }
-
 
     private static void spinCoins() {
         rt1 = new RotateTransition(Duration.seconds(2), coin1);
@@ -348,42 +416,50 @@ public class OG extends Application {
             String result = getCoinResult();
             String result2 = getCoinResult2();
 
+            //updateCoinImages(headsCoin1, result.equals("heads"), result2.equals("heads"));
 
             if (result.equals("heads") && result2.equals("heads") && choice.equals("heads")) {
                 winCount++;
                 score.setText("Wins " + winCount);
-                coin1.setFill(new ImagePattern(new Image("C:\\Users\\Sho\\IdeaProjects\\TwoUp\\src\\main\\resources\\TwoUp\\img\\gold1.png"), 0, 0, 1, 1, true));
-                coin2.setFill(new ImagePattern(new Image("C:\\Users\\Sho\\IdeaProjects\\TwoUp\\src\\main\\resources\\TwoUp\\img\\gold1.png"), 0, 0, 1, 1, true));
+                headsCoin1 = true;  // Set heads for coin1
+                headsCoin2 = true;  // Set heads for coin2
+                updateCoinImages(headsCoin1, headsCoin2);
                 showAlertWithAnimation("Flip Result", "You Win!", "C:\\Users\\Sho\\IdeaProjects\\TwoUp\\src\\main\\resources\\TwoUp\\img\\winner.gif");
             } else if (result.equals("tails") && result2.equals("tails") && choice.equals("tails")) {
                 winCount++;
                 score.setText("Wins " + winCount);
-                coin1.setFill(new ImagePattern(new Image("C:\\Users\\Sho\\IdeaProjects\\TwoUp\\src\\main\\resources\\TwoUp\\img\\gold2.png"), 0, 0, 1, 1, true));
-                coin2.setFill(new ImagePattern(new Image("C:\\Users\\Sho\\IdeaProjects\\TwoUp\\src\\main\\resources\\TwoUp\\img\\gold2.png"), 0, 0, 1, 1, true));
+                headsCoin1 = false;  // Set tails for coin1
+                headsCoin2 = false;  // Set tails for coin2
+                updateCoinImages(headsCoin1, headsCoin2);
                 showAlertWithAnimation("Flip Result", "You Win!", "C:\\Users\\Sho\\IdeaProjects\\TwoUp\\src\\main\\resources\\TwoUp\\img\\winner.gif");
             } else if (result.equals("tails") && result2.equals("tails") && choice.equals("heads")) {
                 loseCount++;
                 score2.setText("Losses " + loseCount);
-                coin1.setFill(new ImagePattern(new Image("C:\\Users\\Sho\\IdeaProjects\\TwoUp\\src\\main\\resources\\TwoUp\\img\\gold2.png"), 0, 0, 1, 1, true));
-                coin2.setFill(new ImagePattern(new Image("C:\\Users\\Sho\\IdeaProjects\\TwoUp\\src\\main\\resources\\TwoUp\\img\\gold2.png"), 0, 0, 1, 1, true));
+                headsCoin1 = false;  // Set tails for coin1
+                headsCoin2 = false;  // Set tails for coin2
+                updateCoinImages(headsCoin1, headsCoin2);
                 showAlertWithAnimation("Flip Result", "You Lose!", "C:\\Users\\Sho\\IdeaProjects\\TwoUp\\src\\main\\resources\\TwoUp\\img\\loser.gif");
             } else if (result.equals("heads") && result2.equals("heads") && choice.equals("tails")) {
                 loseCount++;
                 score2.setText("Losses " + loseCount);
-                coin1.setFill(new ImagePattern(new Image("C:\\Users\\Sho\\IdeaProjects\\TwoUp\\src\\main\\resources\\TwoUp\\img\\gold1.png"), 0, 0, 1, 1, true));
-                coin2.setFill(new ImagePattern(new Image("C:\\Users\\Sho\\IdeaProjects\\TwoUp\\src\\main\\resources\\TwoUp\\img\\gold1.png"), 0, 0, 1, 1, true));
+                headsCoin1 = true;  // Set heads for coin1
+                headsCoin2 = true;  // Set heads for coin2
+                updateCoinImages(headsCoin1, headsCoin2);
                 showAlertWithAnimation("Flip Result", "You Lose!", "C:\\Users\\Sho\\IdeaProjects\\TwoUp\\src\\main\\resources\\TwoUp\\img\\loser.gif");
             } else if (result.equals("tails") && result2.equals("heads") && choice.equals("tails")) {
-                coin1.setFill(new ImagePattern(new Image("C:\\Users\\Sho\\IdeaProjects\\TwoUp\\src\\main\\resources\\TwoUp\\img\\gold2.png"), 0, 0, 1, 1, true));
-                coin2.setFill(new ImagePattern(new Image("C:\\Users\\Sho\\IdeaProjects\\TwoUp\\src\\main\\resources\\TwoUp\\img\\gold1.png"), 0, 0, 1, 1, true));
+                headsCoin1 = false;  // Set tails for coin1
+                headsCoin2 = true;  // Set heads for coin2
+                updateCoinImages(headsCoin1, headsCoin2);
                 showAlert("Flip Again", "Flip again!");
             } else if (result.equals("tails") && result2.equals("heads") && choice.equals("heads")) {
-                coin1.setFill(new ImagePattern(new Image("C:\\Users\\Sho\\IdeaProjects\\TwoUp\\src\\main\\resources\\TwoUp\\img\\gold2.png"), 0, 0, 1, 1, true));
-                coin2.setFill(new ImagePattern(new Image("C:\\Users\\Sho\\IdeaProjects\\TwoUp\\src\\main\\resources\\TwoUp\\img\\gold1.png"), 0, 0, 1, 1, true));
+                headsCoin1 = false;  // Set tails for coin1
+                headsCoin2 = true;  // Set heads for coin2
+                updateCoinImages(headsCoin1, headsCoin2);
                 showAlert("Flip Again", "Flip again!");
             } else {
-                coin1.setFill(new ImagePattern(new Image("C:\\Users\\Sho\\IdeaProjects\\TwoUp\\src\\main\\resources\\TwoUp\\img\\gold1.png"), 0, 0, 1, 1, true));
-                coin2.setFill(new ImagePattern(new Image("C:\\Users\\Sho\\IdeaProjects\\TwoUp\\src\\main\\resources\\TwoUp\\img\\gold2.png"), 0, 0, 1, 1, true));
+                headsCoin1 = true;  // Set heads for coin1
+                headsCoin2 = false;  // Set tails for coin2
+                updateCoinImages(headsCoin1, headsCoin2);
                 showAlert("Flip Again", "Flip again!");
             }
             //ogController controller = new ogController();
@@ -394,33 +470,39 @@ public class OG extends Application {
         delay.play();
     }
 
-    private static void showAlert(String title, String message) {
+
+
+private static void updateCoinImages(boolean headsCoin1, boolean headsCoin2) {
+        coin1.setFill(new ImagePattern(headsCoin1 ? headsImage1 : tailsImage1));
+        coin2.setFill(new ImagePattern(headsCoin2 ? headsImage1 : tailsImage1));
+        }
+
+private static void showAlert(String title, String message) {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle(title);
         alert.setHeaderText(null);
         alert.setContentText(message);
         alert.show();
-    }
+        }
 
-    private static String saveScoresToDatabase() {
-        //ogController controller = new ogController();
+private static String saveScoresToDatabase() {
         String username = LoginData.getUsername();
 
         try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
-            String query = "INSERT INTO scores (player, wins, losses, highscore) VALUES (?, ?, ?, ?)";
-            PreparedStatement statement = connection.prepareStatement(query);
-            statement.setString(1, username);
-            statement.setInt(2, winCount);
-            statement.setInt(3, loseCount);
-            statement.setInt(4, winCount - loseCount);
-            statement.executeUpdate();
+        String query = "INSERT INTO scores (player, wins, losses, highscore) VALUES (?, ?, ?, ?)";
+        PreparedStatement statement = connection.prepareStatement(query);
+        statement.setString(1, username);
+        statement.setInt(2, winCount);
+        statement.setInt(3, loseCount);
+        statement.setInt(4, winCount - loseCount);
+        statement.executeUpdate();
         } catch (SQLException e) {
-            showAlert("Database Error", "Failed to save scores to the database: " + e.getMessage());
+        showAlert("Database Error", "Failed to save scores to the database: " + e.getMessage());
         }
         return username;
-    }
+        }
 
-    private static void showAlertWithAnimation(String title, String message, String gifFileName) {
+private static void showAlertWithAnimation(String title, String message, String gifFileName) {
         Alert alert = new Alert(Alert.AlertType.NONE);
         alert.setTitle(title);
         alert.setHeaderText(null);
@@ -437,23 +519,23 @@ public class OG extends Application {
 
         PauseTransition delay = new PauseTransition(Duration.seconds(3));
         delay.setOnFinished(e -> {
-            alert.setResult(ButtonType.CLOSE);
-            alert.close();
+        alert.setResult(ButtonType.CLOSE);
+        alert.close();
         });
 
         alert.show();
         delay.play();
-    }
+        }
 
-    private static String getCoinResult() {
+private static String getCoinResult() {
         return Math.random() < 0.5 ? "heads" : "tails";
+        }
 
-    }
-    private static String getCoinResult2() {
+private static String getCoinResult2() {
         return Math.random() < 0.5 ? "heads" : "tails";
-    }
+        }
 
-    public static void main(String[] args) {
+public static void main(String[] args) {
         launch(args);
-    }
-}
+        }
+        }
